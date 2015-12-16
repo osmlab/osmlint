83fc189615fb677ec74d2ea961a487d895e05b96
Rub21 <ruben@mapbox.com>
2015-12-16 16:43:37 +0530

    fix untagged ways processor

--- processors/untaggedWays/map.js
+++ processors/untaggedWays/map.js
@@ -1,17 +1,14 @@
 'use strict'
 var _ = require('underscore');
-
 module.exports = function(tileLayers, tile, writeData, done) {
     var layer = tileLayers.osm.osm;
     var result = layer.features.filter(function(val) {
-        if (_.size(val.properties) === 6 && (val.geometry.type === 'LineString')) {
-            return true;
-        }
+        return _.allKeys(val.properties).filter(function(k) {
+            return k.charAt(0) != '_';
+        }).length == 0;
     });
-
     if (result.length > 0) {
         writeData(JSON.stringify(result) + '\n');
     }
-
     done(null, null);
 };
