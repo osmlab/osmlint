2f6a6924cc0d5aa07da55cf714ea8797664752bd
Sajjad Anwar <me@sajjad.in>
2015-12-16 13:02:20 +0530

    require untagged ways

--- index.js
+++ index.js
@@ -3,10 +3,7 @@
 module.exports = {
     'bridgeOnNode': require('./processors/bridge-on-a-node'),
     'filterTime': require('./processors/filter-time'),
+    'filterUsers': require('./processors/filter-users'),
+    'missingLayerBridges': require('./processors/missing-layer-bridges'),
+    'untaggedWays': require('./processors/untagged-ways')
 };
-// require('./processors/duplicated-nodes');
-// require('./processors/filter-time');
-// require('./processors/filter-users');
-// require('./processors/missing-highway-names-us');
-// require('./processors/missing-layer-bridges');
-// require('./processors/untagged-ways');
