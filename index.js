852941fe2c74b1d206cce2c49696bd4a42558159
lxbarth <alex@mapbox.com>
2015-12-16 12:12:13 +0530

    Start refactoring into submodules.

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+require('./processors/bridge-on-a-node');
+require('./processors/duplicated-nodes');
+require('./processors/filter-time');
+require('./processors/filter-users');
+require('./processors/missing-highway-names-us');
+require('./processors/missing-layer-bridges');
+require('./processors/untagged-ways');
