a81ce76d954b1e88952785f4d67c8543222c7920
lxbarth <alex@mapbox.com>
2015-12-16 12:37:58 +0530

    Add tests.

---
+++ test/test.js
@@ -0,0 +1,4 @@
+var processors = require('../index.js');
+var bbox = [-122.05862045288086, 36.93768132842635, -121.97296142578124, 37.00378647456494];
+
+processors.bridgeOnNode('./osm.mbtiles', bbox);
