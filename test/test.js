3a0f21dad1352c6b303b3261e561b363e266b709
Rub21 <ruben@mapbox.com>
2015-12-16 16:41:03 +0530

    test monaco.mbtiles

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 var processors = require('../index.js');
 var bbox = [7.4068451, 43.723259, 7.4422073, 43.752901];
 
-processors.bridgeOnNode(__dirname + '/osm.mbtiles', bbox);
-processors.filterDate(__dirname + '/osm.mbtiles', bbox);
-processors.filterUsers(__dirname + '/osm.mbtiles', bbox);
-// needs TIGER fixture processors.missingHighwaysUS(__dirname + '/osm.mbtiles', __dirname + '/tiger.mbtiles', bbox);
-processors.missingLayerBridges(__dirname + '/osm.mbtiles', bbox);
-processors.untaggedWays(__dirname + '/osm.mbtiles', bbox);
+processors.bridgeOnNode(__dirname + '/monaco.mbtiles', bbox);
+processors.filterDate(__dirname + '/monaco.mbtiles', bbox);
+processors.filterUsers(__dirname + '/monaco.mbtiles', bbox);
+// needs TIGER fixture processors.missingHighwaysUS(__dirname + '/monaco.mbtiles', __dirname + '/tiger.mbtiles', bbox);
+processors.missingLayerBridges(__dirname + '/monaco.mbtiles', bbox);
+processors.untaggedWays(__dirname + '/monaco.mbtiles', bbox);
