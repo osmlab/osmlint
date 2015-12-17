715c7ab8239b424503bdf3943c4234624ba86ff6
lxbarth <alex@mapbox.com>
2015-12-17 14:26:42 +0530

    Sketch in tests.

--- test/test.js
+++ test/test.js
@@ -8,22 +8,18 @@ var processors = require('../index.js');
 var bbox = [7.4068451, 43.723259, 7.4422073, 43.752901];
 var mbtiles = path.join(__dirname, '/data/monaco.mbtiles');
 
-test('Filter Users', function(t) {
-  var result = fs.readFileSync(path.join(__dirname, '/output/filterUsers.json'), 'utf-8');
-  var output = processors.filterUsers(mbtiles, bbox, function() {
-    t.equal(result, output, 'equal');
-    t.end();
-  });
-
+test('bridgeOnNode', function(t) {
+    processors.bridgeOnNode(__dirname + '/fixtures/monaco.mbtiles', bbox, t.end);
 });
-
-
-// var processors = require('../index.js');
-// var bbox = [7.4068451, 43.723259, 7.4422073, 43.752901];
-
-// processors.bridgeOnNode(__dirname + '/monaco.mbtiles', bbox);
-// processors.filterDate(__dirname + '/monaco.mbtiles', bbox);
-//processors.filterUsers(__dirname + '/monaco.mbtiles', bbox);
-// needs TIGER fixture processors.missingHighwaysUS(__dirname + '/monaco.mbtiles', __dirname + '/tiger.mbtiles', bbox);
-// processors.missingLayerBridges(__dirname + '/monaco.mbtiles', bbox);
-// processors.untaggedWays(__dirname + '/monaco.mbtiles', bbox);
+test('filterDate', function(t) {
+    processors.filterDate(__dirname + '/fixtures/monaco.mbtiles', bbox, t.end);
+});
+test('filterUsers', function(t) {
+    processors.filterUsers(__dirname + '/fixtures/monaco.mbtiles', bbox, t.end);
+});
+test('missingLayerBridges', function(t) {
+    processors.missingLayerBridges(__dirname + '/fixtures/monaco.mbtiles', bbox, t.end);
+});
+test('untaggedWays', function(t) {
+    processors.untaggedWays(__dirname + '/fixtures/monaco.mbtiles', bbox, t.end);
+});
