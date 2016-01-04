eca6d3c0f80e9bcd5c3030256acfcf25c7990e89
Rub21 <ruben@mapbox.com>
2016-01-04 16:14:47 -0500

    add self intersecting highways validator

---
+++ validators/selfIntersecting/map.js
@@ -0,0 +1,32 @@
+'use strict'
+var turf = require('turf');
+
+module.exports = function(tileLayers, tile, writeData, done) {
+  var layer = tileLayers.osm.osm;
+  var result = layer.features.filter(function(val) {
+    val.properties._osmlint = 'selfintersecting';
+    return (val.properties.highway && val.geometry.type === 'LineString');
+  });
+  result = result.filter(function(road) {
+    var coordinates = road.geometry.coordinates;
+    var firts_coor = coordinates[0];
+    var last_coor = coordinates[coordinates.length - 1];
+    if (firts_coor[0] === last_coor[0] && firts_coor[1] === last_coor[1]) {
+      return false;
+    }
+    var map = {};
+    var flag = false;
+    coordinates.forEach(function(coor) {
+      if (map[coor.join(',')]) {
+        flag = true;
+      }
+      map[coor.join(',')] = true;
+    });
+    return flag;
+  });
+  if (result.length > 0) {
+    var fc = turf.featurecollection(result);
+    writeData(JSON.stringify(fc) + '\n');
+  }
+  done(null, null);
+};
