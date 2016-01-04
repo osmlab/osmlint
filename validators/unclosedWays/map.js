18cd7d3734dc222d972a66829455b8ab4ece86a1
Rub21 <ruben@mapbox.com>
2016-01-04 18:37:55 -0500

    add preserve type area

--- validators/unclosedWays/map.js
+++ validators/unclosedWays/map.js
@@ -5,7 +5,8 @@ module.exports = function(tileLayers, tile, writeData, done) {
   var layer = tileLayers.osm.osm;
   var result = layer.features.filter(function(val) {
     val.properties._osmlint = 'unclosedWays';
-    if (val.geometry.type === 'LineString' && (val.properties.landuse || val.properties.building)) {
+    var preserve_type = (val.properties.aeroway || val.properties.landuse || val.properties.building || val.properties.leisure); // no val.properties.natural
+    if (val.geometry.type === 'LineString' && preserve_type) {
       var coordinates = val.geometry.coordinates;
       var first_coor = coordinates[0];
       var last_coor = coordinates[coordinates.length - 1];
@@ -13,11 +14,6 @@ module.exports = function(tileLayers, tile, writeData, done) {
         return false;
       }
       return true;
-      // var first_point = turf.point(first_coor);
-      // var point_last = turf.point(last_coor);
-      // var units = "kilometers";
-      // var distance = turf.distance(first_point, point_last, units) / 1000; //meters
-      // return (distance < 5); //return ways wich are unclosed 
     }
   });
 
