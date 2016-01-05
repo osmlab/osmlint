3b996660ffb57ce2ecfcf786c6a350017060670a
Rub21 <ruben@mapbox.com>
2016-01-05 17:16:32 -0500

    simplify self-intersecting

--- validators/selfIntersecting/map.js
+++ validators/selfIntersecting/map.js
@@ -7,14 +7,12 @@ module.exports = function(tileLayers, tile, writeData, done) {
     val.properties._osmlint = 'selfintersecting';
     return (val.properties.highway && val.geometry.type === 'LineString');
   });
-  result = result.filter(function(road) {
-    var coordinates = road.geometry.coordinates;
-    for (var i = 0; i < coordinates.length - 1; i++) {
-      var line = turf.linestring([coordinates[i], coordinates[i + 1]]);
-      var intersect = turf.intersect(line, road);
-      if (intersect.geometry.type !== 'LineString') {
-        return true;
-      }
+  result = result.filter(function(val) {
+    var highway = val;
+    var road_length = val.geometry.coordinates.length;
+    var intersect = turf.intersect(highway, val);
+    if (intersect.geometry.coordinates.length > road_length) {
+      return true;
     }
   });
   if (result.length > 0) {
