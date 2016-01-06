e6bd4d89cd796b33767176d5894bb4a74cf4a9f7
Rub21 <ruben@mapbox.com>
2016-01-06 10:13:01 -0500

    value unclosed area

--- validators/unclosedWays/map.js
+++ validators/unclosedWays/map.js
@@ -1,5 +1,6 @@
 'use strict'
 var turf = require('turf');
+var preserve_type = require('./value_area');
 
 module.exports = function(tileLayers, tile, writeData, done) {
   var layer = tileLayers.osm.osm;
@@ -7,17 +8,23 @@ module.exports = function(tileLayers, tile, writeData, done) {
   var bboxlinestring = turf.bboxPolygon(bbox);
   bboxlinestring.geometry.type = 'LineString';
   bboxlinestring.geometry.coordinates = bboxlinestring.geometry.coordinates[0];
-  var buffer = turf.buffer(bboxlinestring, 0.0001, 'miles').features[0];
+  var buffer = turf.buffer(bboxlinestring, 0.0005, 'miles').features[0];
+
   var result = layer.features.filter(function(val) {
     val.properties._osmlint = 'unclosedWays';
-    var preserve_type = (val.properties.aeroway || val.properties.landuse || val.properties.building || val.properties.leisure);
-    if (val.geometry.type === 'LineString' && preserve_type) {
+    var value_type = (
+      preserve_type.area[val.properties.area] ||
+      preserve_type.building[val.properties.building] ||
+      preserve_type.landuse[val.properties.landuse] ||
+      preserve_type.aeroway[val.properties.aeroway] ||
+      preserve_type.leisure[val.properties.leisure] ||
+      preserve_type.natural[val.properties.natural] ||
+      preserve_type.man_made[val.properties.man_made]);
+
+    if (val.geometry.type === 'LineString' && value_type) {
       var coordinates = val.geometry.coordinates;
       var first_coor = coordinates[0];
       var last_coor = coordinates[coordinates.length - 1];
-      if (first_coor[0] === last_coor[0] && first_coor[1] === last_coor[1]) {
-        return false;
-      }
       if (turf.inside(turf.point(first_coor), buffer) || turf.inside(turf.point(last_coor), buffer)) {
         return false;
       }
