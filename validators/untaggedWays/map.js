ed762db2f5e5ca21696fdef87107ea4f2fb5c0c6
Rub21 <ruben@mapbox.com>
2016-01-13 10:06:25 -0500

    simplify untagedways, dont consider for closed ways

--- validators/untaggedWays/map.js
+++ validators/untaggedWays/map.js
@@ -6,8 +6,7 @@ var turf = require('turf');
 // Find untagged ways.
 module.exports = function(tileLayers, tile, writeData, done) {
   var layer = tileLayers.osm.osm;
-  var result = [];
-  var values = layer.features.filter(function(val) {
+  var result = layer.features.filter(function(val) {
     var hasKeys = _.allKeys(val.properties).filter(function(k) {
       return k.charAt(0) !== '_';
     }).length === 0;
@@ -17,40 +16,14 @@ module.exports = function(tileLayers, tile, writeData, done) {
     }
   });
 
-  values = values.filter(function(value) {
+  result = result.filter(function(value) {
     var firstCoord = value.geometry.coordinates[0];
     var endCoord = value.geometry.coordinates[value.geometry.coordinates.length - 1];
-    if (firstCoord[0] === endCoord[0] && firstCoord[0] === endCoord[0]) {
-      value.geometry.type = 'Polygon';
-      value.geometry.coordinates = [value.geometry.coordinates];
-      var kinks = turf.kinks(value);
-      if (kinks.intersections.features.length === 0) {
-        return false;
-      }
+    if (firstCoord[0] === endCoord[0] && firstCoord[1] === endCoord[1]) {
+      return false;
     }
     return true;
   });
-
-  while (values.length > 0) {
-    var value = values[0];
-    values.splice(0, 1);
-    if (value.geometry.value === 'Polygon') {
-      var flag = true;
-      for (var j = 0; j < values.length; j++) {
-        if (values[j].geometry.value === 'Polygon') {
-          var intersect = turf.intersect(value, values[j]);
-          if (intersect !== undefined) {
-            flag = false;
-          }
-        }
-      }
-      if (flag) {
-        result.push(value);
-      }
-    }
-    result.push(value);
-  }
-
 
   var fc = turf.featurecollection(result);
   writeData(JSON.stringify(fc) + '\n');
