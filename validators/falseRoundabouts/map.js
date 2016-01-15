e3c352dd238b63a895083b253f6e58ed1c601f5f
Sajjad Anwar <me@sajjad.in>
2016-01-15 22:49:24 +0530

    only look at LineStrings

--- validators/falseRoundabouts/map.js
+++ validators/falseRoundabouts/map.js
@@ -4,7 +4,7 @@ var turf = require('turf');
 module.exports = function(tileLayers, tile, writeData, done) {
   var layer = tileLayers.osm.osm;
   var result = layer.features.filter(function(val) {
-    if (val.properties.junction && val.properties.junction === 'roundabout') {
+    if (val.properties.junction && val.properties.junction === 'roundabout' && val.geometry.type === 'LineString') {
       var coordinates = val.geometry.coordinates;
       var coordinatesLength = coordinates.length;
       var start = turf.point(coordinates[0]);
