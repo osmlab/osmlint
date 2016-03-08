0b7d6dfdc3021ec8b56782c565ac01fe2843eb91
Rub21 <ruben@mapbox.com>
2016-03-08 18:04:48 -0500

    classification of roads overlaphighways

--- validators/overlapHighways/map.js
+++ validators/overlapHighways/map.js
@@ -7,23 +7,37 @@ module.exports = function(tileLayers, tile, writeData, done) {
   var layer = tileLayers.osm.osm;
   var highways = {};
   var bboxes = [];
-  var preserveType = {
+  var majorRoads = {
     'motorway': true,
-    'motorway_link': true,
-    'primary': true,
-    'primary_link': true,
-    'secondary': true,
-    'secondary_link': true,
-    'tertiary': true,
-    'tertiary_link': true,
     'trunk': true,
+    'primary': true,
+    'secondary': true,
+    'tertiary': true,
+    'motorway_link': true,
     'trunk_link': true,
-    'residential': true,
-    'unclassified': true,
-    'living_street': true,
-    'road': true,
-    'service': true
+    'primary_link': true,
+    'secondary_link': true,
+    'tertiary_link': true
   };
+  var minorRoads = {
+    'unclassified': true,
+    'residential': true,
+    'living_street': true,
+    'service': true,
+    'road': true
+  };
+  var pathRoads = {
+    'pedestrian': true,
+    'track': true,
+    'footway': true,
+    'path': true,
+    'cycleway': true,
+    'steps': true
+  };
+  var preserveType = {};
+  preserveType = _.extend(preserveType, majorRoads);
+  preserveType = _.extend(preserveType, minorRoads);
+  //preserveType = _.extend(preserveType, pathRoads);
   var osmlint = 'overlaphighways';
 
   for (var i = 0; i < layer.features.length; i++) {
@@ -44,13 +58,31 @@ module.exports = function(tileLayers, tile, writeData, done) {
     for (var k = 0; k < overlaps.length; k++) {
       var overlap = overlaps[k];
       if (bbox[4] !== overlap[4]) {
-        var intersect = turf.intersect(highways[overlap[4]], highways[bbox[4]]);
+        var fromHighway = highways[bbox[4]];
+        var toHighway = highways[overlap[4]];
+        var intersect = turf.intersect(toHighway, fromHighway);
         if (intersect !== undefined && (intersect.geometry.type === 'LineString' || intersect.geometry.type === 'MultiLineString')) {
           var coordinates = intersect.geometry.coordinates;
+          var type;
+          if (majorRoads[fromHighway.properties.highway] && majorRoads[toHighway.properties.highway]) {
+            type = 'major-major';
+          } else if ((majorRoads[fromHighway.properties.highway] && minorRoads[toHighway.properties.highway]) || (minorRoads[fromHighway.properties.highway] && majorRoads[toHighway.properties.highway])) {
+            type = 'major-minor';
+          } else if ((majorRoads[fromHighway.properties.highway] && pathRoads[toHighway.properties.highway]) || (pathRoads[fromHighway.properties.highway] && majorRoads[toHighway.properties.highway])) {
+            type = 'major-path';
+          } else if (minorRoads[fromHighway.properties.highway] && minorRoads[toHighway.properties.highway]) {
+            type = 'minor-minor';
+          } else if ((minorRoads[fromHighway.properties.highway] && pathRoads[toHighway.properties.highway]) || (pathRoads[fromHighway.properties.highway] && minorRoads[toHighway.properties.highway])) {
+            type = 'minor-path';
+          } else if (pathRoads[fromHighway.properties.highway] && pathRoads[toHighway.properties.highway]) {
+            type = 'path-path';
+          }
+
           var props = {
             _osmlint: osmlint,
             _fromWay: bbox[4],
-            _toWay: overlap[4]
+            _toWay: overlap[4],
+            _type: type
           };
           if (intersect.geometry.type === 'MultiLineString') {
             for (var l = 0; l < coordinates.length; l++) {
@@ -62,10 +94,10 @@ module.exports = function(tileLayers, tile, writeData, done) {
             output[coordinates[0]] = turf.point(coordinates[0], props);
             output[coordinates[coordinates.length - 1]] = turf.point(coordinates[coordinates.length - 1], props);
           }
-          highways[bbox[4]].properties._osmlint = osmlint;
-          highways[overlap[4]].properties._osmlint = osmlint;
-          output[bbox[4]] = highways[bbox[4]];
-          output[overlap[4]] = highways[overlap[4]];
+          fromHighway.properties._osmlint = osmlint;
+          toHighway.properties._osmlint = osmlint;
+          output[bbox[4]] = fromHighway;
+          output[overlap[4]] = toHighway;
         }
       }
     }
