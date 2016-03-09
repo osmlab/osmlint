5ffa7d07172acc934bb0da04ba8a90933254ffad
Rub21 <ruben@mapbox.com>
2016-03-09 10:41:08 -0500

    name cafe on amenity

---
+++ validators/nameCafeOnAmenity/map.js
@@ -0,0 +1,20 @@
+'use strict';
+var turf = require('turf');
+
+module.exports = function(tileLayers, tile, writeData, done) {
+  var layer = tileLayers.osm.osm;
+  var result = layer.features.filter(function(val) {
+    if (val.properties.amenity == 'cafe' && val.properties.name && val.properties.name.toLowerCase().indexOf('cafe') !== -1) {
+
+      val.properties._osmlint = 'namecafeonamenity';
+      return true;
+    }
+  });
+
+  if (result.length > 0) {
+    var fc = turf.featurecollection(result);
+    writeData(JSON.stringify(fc) + '\n');
+  }
+
+  done(null, null);
+};
