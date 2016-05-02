7c216021922e3092689a378a3fd7f9b94a92bc56
Rub21 <ruben@mapbox.com>
2016-05-02 11:26:18 -0500

    rename

--- index.js
+++ index.js
@@ -15,5 +15,6 @@ module.exports = {
   'islandsHighways': require('./validators/islandsHighways'),
   'overlapHighways': require('./validators/overlapHighways'),
   'impossibleAngle': require('./validators/impossibleAngle'),
-  'tigerDelta': require('./validators/tigerDelta')
+  'tigerDelta': require('./validators/tigerDelta'),
+  'impossibleOneWays': require('./validators/impossibleoneways')
 };
--- validators/imposibleOneWays/README.md
+++
@@ -1,9 +0,0 @@
-#### Description
-
-This validator detects imposible one ways.
-
-
-#### Usage
-
-1. Download [OSM QA Tiles](https://osmlab.github.io/osm-qa-tiles/) for the planet or a country that you are interested in. 
-2. For example, to run for peru.mbtiles: ` osmlint imposibleoneways --bbox="[-85.348663,-19.664209,-64.606476,1.3831570]" peru.mbtiles`
