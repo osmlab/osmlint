118826e8835da31b031e989c34391ae2c1c9c41a
Rub21 <ruben@mapbox.com>
2016-05-06 18:24:19 -0500

    avoid tuner in waterway

--- validators/crossingWaterwaysHighways/map.js
+++ validators/crossingWaterwaysHighways/map.js
@@ -45,7 +45,8 @@ module.exports = function(tileLayers, tile, writeData, done) {
     'dam': true,
     'weir': true,
     'waterfall': true,
-    'stream': true
+    'stream': true,
+    'ditch': true
   };
   var fords = {};
   var osmlint = 'crossingwaterwayshighways';
@@ -53,7 +54,7 @@ module.exports = function(tileLayers, tile, writeData, done) {
     var val = layer.features[i];
     var id = val.properties._osm_way_id;
     var bbox;
-    if (preserveType[val.properties.highway] && !val.properties.bridge && !val.properties.tunnel && !val.properties.ford) {
+    if (preserveType[val.properties.highway] && val.properties.bridge === undefined && val.properties.tunnel === undefined && val.properties.ford === undefined) {
       if (val.geometry.type === 'LineString') {
         var idWayL = id + 'L';
         bbox = turf.extent(val);
@@ -73,7 +74,7 @@ module.exports = function(tileLayers, tile, writeData, done) {
           }
         }
       }
-    } else if (((val.properties.waterway && !dontPreserveWaterways[val.properties.waterway]) || val.properties.natural === 'water') && (val.geometry.type === 'LineString' || val.geometry.type === 'Polygon')) {
+    } else if (((val.properties.waterway && !dontPreserveWaterways[val.properties.waterway]) || val.properties.natural === 'water') && (val.geometry.type === 'LineString' || val.geometry.type === 'Polygon') && val.properties.tunnel === undefined) {
       if (val.geometry.type === 'Polygon') {
         val.geometry.type = 'LineString';
         val.geometry.coordinates = val.geometry.coordinates[0];
