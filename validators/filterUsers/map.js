32eb4320649fe4a812e437b45ef2549e9d7ce13e
Rub21 <ruben@mapbox.com>
2016-05-23 16:33:10 -0500

    update filterUsers

--- validators/filterUsers/map.js
+++ validators/filterUsers/map.js
@@ -6,7 +6,7 @@ var users = require('./users.json');
 module.exports = function(tileLayers, tile, writeData, done) {
   var layer = tileLayers.osm.osm;
   var result = layer.features.filter(function(val) {
-    return users.hasOwnProperty(val.properties._user);
+    return users.hasOwnProperty(val.properties['@user']);
   });
 
   if (result.length > 0) {
