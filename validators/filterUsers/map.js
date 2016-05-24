f5044bc5f3b2ff5c942053a46657ba1fa7010dce
arunasank <aruna@mapbox.com>
2016-05-24 19:57:01 +0530

    Use the mapbox-data-team npm module

--- validators/filterUsers/map.js
+++ validators/filterUsers/map.js
@@ -1,12 +1,12 @@
 'use strict';
 var turf = require('turf');
-var users = require('./users.json');
+var users = require('mapbox-data-team').getUsernames();
 
 // Filter features touched by list of users defined by users.json
 module.exports = function(tileLayers, tile, writeData, done) {
   var layer = tileLayers.osm.osm;
   var result = layer.features.filter(function(val) {
-    return users.hasOwnProperty(val.properties._user);
+    return (users.indexOf(val.properties._user) > -1);
   });
 
   if (result.length > 0) {
