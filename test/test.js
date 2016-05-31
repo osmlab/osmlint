021a583249a640da1b92b4e8d33e45915399a6d0
arunasank <aruna@mapbox.com>
2016-05-31 16:49:48 +0530

    Old monaco tiles has karitotp

--- test/test.js
+++ test/test.js
@@ -58,7 +58,7 @@ test('filterUsers', function(t) {
       var geoJSON = JSON.parse(logs[i]);
       t.comment('Pass: ' + (i + 1));
       t.equal(GJV.isFeatureCollection(geoJSON), true, 'Should be a FeatureCollection');
-      t.equal(geoJSON.features[0].properties['@user'], 'Rub21', 'Should be Rub21');
+      t.equal(geoJSON.features[0].properties['@user'], 'karitotp', 'Should be karitotp');
     }
     t.end();
   });
