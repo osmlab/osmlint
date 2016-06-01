5f99d8229f81fb55322f668923da48a9bd3feb7e
arunasank <aruna@mapbox.com>
2016-06-01 20:30:45 +0530

    Reorder tests

--- test/test.js
+++ test/test.js
@@ -31,7 +31,7 @@ var commonOpts = {
   zoom: zoom
 };
 
-// Parameters for testing missingHighwaysUS
+//Parameters for testing missingHighwaysUS
 var osmLevyCountyTiles = path.join(__dirname, '/fixtures/osm.levycounty.mbtiles');
 var tiger2015LevyCountyTiles = path.join(__dirname, '/fixtures/tiger2015.levycounty.mbtiles');
 var optsMissingHighwaysUS = {
@@ -125,40 +125,6 @@ test('unclosedWays', function(t) {
       t.comment('Pass: ' + (i + 1));
       if (geoJSON.features.length > 0) {
         t.equal(geoJSON.features[0].properties._osmlint, 'unclosedways', 'Should be unclosedways');
-        t.equal(geoJSON.features[0].geometry.type, 'LineString', 'Should be  LineString');
-      }
-    }
-    t.end();
-  });
-});
-
-test('untaggedWays', function(t) {
-  t.plan(2);
-  logInterceptor();
-  processors.untaggedWays(optsMissingHighwaysUS, osmLevyCountyTiles, function() {
-    var logs = logInterceptor.end();
-    for (var i = 0; i < logs.length; i++) {
-      var geoJSON = JSON.parse(logs[i]);
-      t.comment('Pass: ' + (i + 1));
-      if (geoJSON.features.length > 0) {
-        t.equal(geoJSON.features[0].properties._osmlint, 'untaggedway', 'Should be untaggedway');
-        t.equal(geoJSON.features[0].geometry.type, 'LineString', 'Should be  LineString');
-      }
-    }
-    t.end();
-  });
-});
-
-test('missingHighwaysUS', function(t) {
-  t.plan(54);
-  logInterceptor();
-  processors.missingHighwaysUS(optsMissingHighwaysUS, osmLevyCountyTiles, tiger2015LevyCountyTiles, function() {
-    var logs = logInterceptor.end();
-    for (var i = 0; i < logs.length; i++) {
-      var geoJSON = JSON.parse(logs[i]);
-      t.comment('Pass: ' + (i + 1));
-      if (geoJSON.features.length > 0) {
-        t.equal(geoJSON.features[0].properties._osmlint, 'missinghighwayus', 'Should be missinghighwayus');
         t.equal(geoJSON.features[0].geometry.type, 'LineString', 'Should be  LineString');
       }
     }
@@ -270,23 +236,6 @@ test('impossibleAngle', function(t) {
   });
 });
 
-test('tigerDelta', function(t) {
-  t.plan(102);
-  logInterceptor();
-  processors.tigerDelta(optsMissingHighwaysUS, osmLevyCountyTiles, tiger2015LevyCountyTiles, function() {
-    var logs = logInterceptor.end();
-    for (var i = 0; i < logs.length; i++) {
-      var geoJSON = JSON.parse(logs[i]);
-      t.comment('Pass: ' + (i + 1));
-      if (geoJSON.features.length > 0) {
-        t.equal(geoJSON.features[0].properties._osmlint, 'tigerdelta', 'Should be tigerdelta');
-        t.equal(geoJSON.features[0].geometry.type, 'LineString', 'Should be  LineString');
-      }
-    }
-    t.end();
-  });
-});
-
 test('fixmeTag', function(t) {
   t.plan(2);
   logInterceptor();
@@ -311,6 +260,57 @@ test('impossibleOneWays', function(t) {
     var geoJSON = JSON.parse(logs[0]);
     t.equal(geoJSON.features[0].properties._osmlint, 'impossibleoneways', 'Should be impossibleoneways');
     t.equal(geoJSON.features[0].geometry.type, 'LineString', 'Should be  LineString');
+    t.end();
+  });
+});
+
+test('missingHighwaysUS', function(t) {
+  t.plan(54);
+  logInterceptor();
+  processors.missingHighwaysUS(optsMissingHighwaysUS, osmLevyCountyTiles, tiger2015LevyCountyTiles, function() {
+    var logs = logInterceptor.end();
+    for (var i = 0; i < logs.length; i++) {
+      var geoJSON = JSON.parse(logs[i]);
+      t.comment('Pass: ' + (i + 1));
+      if (geoJSON.features.length > 0) {
+        t.equal(geoJSON.features[0].properties._osmlint, 'missinghighwayus', 'Should be missinghighwayus');
+        t.equal(geoJSON.features[0].geometry.type, 'LineString', 'Should be  LineString');
+      }
+    }
+    t.end();
+  });
+});
+
+test('tigerDelta', function(t) {
+  t.plan(102);
+  logInterceptor();
+  processors.tigerDelta(optsMissingHighwaysUS, osmLevyCountyTiles, tiger2015LevyCountyTiles, function() {
+    var logs = logInterceptor.end();
+    for (var i = 0; i < logs.length; i++) {
+      var geoJSON = JSON.parse(logs[i]);
+      t.comment('Pass: ' + (i + 1));
+      if (geoJSON.features.length > 0) {
+        t.equal(geoJSON.features[0].properties._osmlint, 'tigerdelta', 'Should be tigerdelta');
+        t.equal(geoJSON.features[0].geometry.type, 'LineString', 'Should be  LineString');
+      }
+    }
+    t.end();
+  });
+});
+
+test('untaggedWays', function(t) {
+  t.plan(2);
+  logInterceptor();
+  processors.untaggedWays(optsMissingHighwaysUS, osmLevyCountyTiles, function() {
+    var logs = logInterceptor.end();
+    for (var i = 0; i < logs.length; i++) {
+      var geoJSON = JSON.parse(logs[i]);
+      t.comment('Pass: ' + (i + 1));
+      if (geoJSON.features.length > 0) {
+        t.equal(geoJSON.features[0].properties._osmlint, 'untaggedway', 'Should be untaggedway');
+        t.equal(geoJSON.features[0].geometry.type, 'LineString', 'Should be  LineString');
+      }
+    }
     t.end();
   });
 });
