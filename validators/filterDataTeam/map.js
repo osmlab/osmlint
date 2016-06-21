cd3417c1726d9e9e68f90b6cd6d113419b1db14a
Rub21 <ruben@mapbox.com>
2016-06-20 21:36:33 -0500

    rename

--- validators/filterDataTeam/map.js
+++ validators/filterDataTeam/map.js
@@ -12,8 +12,12 @@ users = users.reduce(function(memo, currentValue) {
 module.exports = function(tileLayers, tile, writeData, done) {
   var layer = tileLayers.osm.osm;
   var result = layer.features.filter(function(val) {
-    return (users.hasOwnProperty(val.properties['@user']) && val.properties['@timestamp'] >= today);
+    if (users.hasOwnProperty(val.properties['@user']) && val.properties['@timestamp'] >= today) {
+      val.properties_osmlint = 'filterdatateam';
+      return true;
+    }
   });
+
   if (result.length > 0) {
     var fc = turf.featurecollection(result);
     writeData(JSON.stringify(fc) + '\n');
