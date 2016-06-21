cca77e5834bcc7f9b8184820d8639f9842bd3fe3
Rub21 <ruben@mapbox.com>
2016-06-20 23:20:21 -0500

    clean up

--- validators/filterDataTeam/map.js
+++ validators/filterDataTeam/map.js
@@ -2,7 +2,7 @@
 var time = require('time')(Date);
 var turf = require('turf');
 var users = require('mapbox-data-team').getUsernames();
-var today = (time.time() - 7 * 24 * 60 * 60);
+var sevenDaysAgo = (time.time() - 7 * 24 * 60 * 60);
 
 users = users.reduce(function(memo, currentValue) {
   memo[currentValue.toString()] = true;
@@ -13,7 +13,7 @@ module.exports = function(tileLayers, tile, writeData, done) {
   var layer = tileLayers.osm.osm;
   var result = layer.features.filter(function(val) {
     val.properties._osmlint = 'filterdatateam';
-    return (users.hasOwnProperty(val.properties['@user']) && val.properties['@timestamp'] >= today);
+    return (users.hasOwnProperty(val.properties['@user']) && val.properties['@timestamp'] >= sevenDaysAgo);
   });
 
   if (result.length > 0) {
