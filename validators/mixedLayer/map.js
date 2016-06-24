dbe94b2a72c8f0458b251e6eabf19e583e2300cd
Rub21 <ruben@mapbox.com>
2016-06-24 07:07:47 -0500

    right detection

--- validators/mixedLayer/map.js
+++ validators/mixedLayer/map.js
@@ -74,50 +74,43 @@ module.exports = function(tileLayers, tile, writeData, done) {
   for (var j = 0; j < highwaybboxes.length; j++) {
     var valueHighway = highways[highwaybboxes[j][4]];
     //check only highways which has layer
-    if (valueHighway.properties.layer) {
+    if (valueHighway.properties.layer && valueHighway.properties.layer !== '0' && valueHighway.geometry.coordinates.length > 2) {
       var overlaphighwaysBbox = highwaysTree.search(highwaybboxes[j]);
       for (var k = 0; k < overlaphighwaysBbox.length; k++) {
-      var overlaphighway = highways[overlaphighwaysBbox[4]];
-      if(valueHighway.properties['@id'] !== overlaphighway.properties['@id']){
-        var intersect = turf.intersect(valueHighway, overlaphighway);
-       if (intersect) {
-        var props = {
-          _fromWay: valueHighway.properties['@id'],
-          _toWay: overlaphighway.properties['@id'],
-          _osmlint: osmlint,
-          _type: classification(majorRoads, minorRoads, pathRoads, valueHighway.properties.highway)
-        };
-        intersect.properties = props;
-        valueHighway.properties._osmlint = osmlint;
-        overlaphighway.properties._osmlint = osmlint;
-        output[valueHighway.properties['@id']] = valueHighway;
-        output[overlaphighway.properties['@id']] = overlaphighway;
-
-
-        if (intersect.geometry.type === 'MultiPoint') {
-          var coord = intersect.geometry.coordinates;
-          for (var l = 0; l < coord.length; l++) {
-            if (!fords[coord[l].join(',')]) {
-              var point = turf.point(coord[l]);
-              point.properties = props;
-              output[waterBbox[4] + overlapHigBbox[4] + l] = point;
+        var overlaphighway = highways[overlaphighwaysBbox[k][4]];
+        if (valueHighway.properties['@id'] !== overlaphighway.properties['@id'] && valueHighway.properties.layer !== overlaphighway.properties.layer && overlaphighway.geometry.coordinates.length > 2) {
+          var coorVH = valueHighway.geometry.coordinates;
+          var coorOH = overlaphighway.geometry.coordinates;
+          var middleCoorVH = _.without(_.flatten(coorVH), coorVH[0][0], coorVH[0][1], coorVH[coorVH.length - 1][0], coorVH[coorVH.length - 1][0]); // valueHighway.geometry.coordinates.splice(0, 1).splice(-1, 1);
+          var middleCoorOH = _.without(_.flatten(coorOH), coorOH[0][0], coorOH[0][1], coorOH[coorOH.length - 1][0], coorOH[coorOH.length - 1][0]); // overlaphighway.geometry.coordinates.splice(0, 1).splice(-1, 1);
+          var inter = _.intersection(middleCoorVH, middleCoorOH);
+          if (inter.length == 2) {
+            var intersect = turf.point(inter);
+            var props = {
+              _fromWay: valueHighway.properties['@id'],
+              _toWay: overlaphighway.properties['@id'],
+              _osmlint: osmlint,
+              _type: classification(majorRoads, minorRoads, pathRoads, valueHighway.properties.highway)
+            };
+            intersect.properties = props;
+            valueHighway.properties._osmlint = osmlint;
+            overlaphighway.properties._osmlint = osmlint;
+            output[valueHighway.properties['@id']] = valueHighway;
+            output[overlaphighway.properties['@id']] = overlaphighway;
+            if (intersect.geometry.type === 'MultiPoint') {
+              var coord = intersect.geometry.coordinates;
+              for (var l = 0; l < coord.length; l++) {
+                var point = turf.point(coord[l]);
+                point.properties = props;
+                output[valueHighway.properties['@id'] + overlaphighway.properties['@id'] + l] = point;
+              }
+            } else if (intersect.geometry.type === 'Point') {
+              output[valueHighway.properties['@id'] + overlaphighway.properties['@id']] = intersect;
             }
           }
-        } else if (intersect.geometry.type === 'Point' && !fords[intersect.geometry.coordinates.join(',')]) {
-          output[waterBbox[4] + overlapHigBbox[4]] = intersect;
         }
       }
-
-      }
-
-
     }
-
-
-    }
-
-
-    
   }
 
   var result = _.values(output);
