bb7f127712248d9ffe92171112e1b36fe15ed625
Rub21 <ruben@mapbox.com>
2016-06-27 22:29:54 -0500

    support MultiLineString

--- validators/selfIntersectingHighways/map.js
+++ validators/selfIntersectingHighways/map.js
@@ -1,11 +1,12 @@
 'use strict';
 var turf = require('turf');
 var _ = require('underscore');
+var flatten = require('geojson-flatten');
 
 // Find self intersecting highways.
 module.exports = function(tileLayers, tile, writeData, done) {
   var layer = tileLayers.osm.osm;
-  var output = [];
+  var highways = [];
   var majorRoads = {
     'motorway': true,
     'trunk': true,
@@ -42,24 +43,32 @@ module.exports = function(tileLayers, tile, writeData, done) {
     var highway = layer.features[i];
     if (preserveType[highway.properties.highway] && highway.geometry.type === 'LineString') {
       if (highway.geometry.coordinates.length > 2) {
-        output.push(highway);
+        highways.push(highway);
+      }
+    } else if (preserveType[highway.properties.highway] && highway.geometry.type === 'MultiLineString') {
+      var arrayWays = flatten(highway);
+      for (var f = 0; f < arrayWays.length; f++) {
+        if (arrayWays[f].geometry.type === 'LineString' && arrayWays[f].geometry.coordinates.length > 2) {
+          arrayWays[f].properties = highway.properties;
+          highways.push(arrayWays[f]);
+        }
       }
     }
   }
+
   var result = [];
 
-  for (var j = 0; j < output.length; j++) {
-    var valueHighway = output[j];
+  for (var j = 0; j < highways.length; j++) {
+    var valueHighway = highways[j];
     var coordLength = valueHighway.geometry.coordinates.length;
     var intersect = turf.intersect(valueHighway, valueHighway);
     if (intersect.geometry.coordinates.length > coordLength) {
-      //objects  to compare
+      //objects to compare
       var objcoords = {};
       var valueCoords = valueHighway.geometry.coordinates;
       var coords = intersect.geometry.coordinates.map(function(v) {
         return v[0];
       });
-
       for (var h = 0; h < coords.length; h++) {
         objcoords[coords[h].join('-')] = coords[h];
       }
@@ -68,25 +77,28 @@ module.exports = function(tileLayers, tile, writeData, done) {
           delete objcoords[valueCoords[w].join('-')];
         }
       }
+      var type = classification(majorRoads, minorRoads, pathRoads, valueHighway.properties.highway);
       var arrcoords = _.values(objcoords);
       for (var g = 0; g < arrcoords.length; g++) {
         var point = turf.point(arrcoords[g]);
         point.properties._osmlint = osmlint;
-        point.properties._way = valueHighway.properties['@id'];
+        point.properties._fromWay = valueHighway.properties['@id'];
+        point.properties._type = type;
         result.push(point);
       }
-      valueHighway.properties._type = classification(majorRoads, minorRoads, pathRoads, valueHighway.properties.highway);
+      valueHighway.properties._type = type;
       valueHighway.properties._osmlint = osmlint;
       result.push(valueHighway);
     }
   }
+
   if (result.length > 0) {
     var fc = turf.featurecollection(result);
     writeData(JSON.stringify(fc) + '\n');
   }
+
   done(null, null);
 };
-
 
 function classification(major, minor, path, highway) {
   if (major[highway]) {
