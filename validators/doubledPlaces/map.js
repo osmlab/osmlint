7beb0d6d0c3a0bdd215feb7c1ca02c5f4ef00438
samely <eliane@mapbox.com>
2016-07-26 17:47:10 -0500

    updating doubledplaces validator

--- validators/doubledPlaces/map.js
+++ validators/doubledPlaces/map.js
@@ -10,7 +10,7 @@ module.exports = function(tileLayers, tile, writeData, done) {
 
   for (var i = 0; i < layer.features.length; i++) {
     var val = layer.features[i];
-    if (val.properties.name !== undefined && (val.geometry.type === 'Point' || val.geometry.type === 'Polygon')) {
+    if (val.properties.highway !== 'bus_stop' && val.properties.railway !== 'station' && val.properties.name !== undefined && (val.geometry.type === 'Point' || val.geometry.type === 'Polygon')) {
       if (!objnames[val.properties.name]) {
         objnames[val.properties.name] = val;
       } else if (objnames[val.properties.name].properties['@id'] !== val.properties['@id'] && objnames[val.properties.name].geometry.type !== val.geometry.type && getDistance(objnames[val.properties.name], val) < 0.03) {
