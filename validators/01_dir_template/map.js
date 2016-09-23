12193a4acc902dbf0d3bf0cef6a81ac22d0610ac
Rub21 <ruben@mapbox.com>
2016-09-23 10:56:43 -0500

    add template to start a validator
    update
    
    fix typo

---
+++ validators/01_dir_template/map.js
@@ -0,0 +1,21 @@
+'use strict';
+var turf = require('turf');
+
+module.exports = function(tileLayers, tile, writeData, done) {
+  var layer = tileLayers.osm.osm;
+  var osmlint = '01_dir_template';
+  var result = [];
+  for (var i = 0; i < layer.features.length; i++) {
+    var val = layer.features[i];
+    //here comes all your code to validate the data
+    val.properties._osmlint = osmlint;
+    result.push(val);
+  }
+
+  if (result.length > 0) {
+    var fc = turf.featurecollection(result);
+    writeData(JSON.stringify(fc) + '\n');
+  }
+
+  done(null, null);
+};
