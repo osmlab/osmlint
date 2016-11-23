d837efeff60182aebf8ee5fab9250383664dd11f
dannykath <danny@mapbox.com>
2016-11-23 16:57:02 -0500

    upload validator

---
+++ validators/missingRoundabout/map.js
@@ -0,0 +1,28 @@
+'use strict';
+var turf = require('turf');
+
+module.exports = function(tileLayers, tile, writeData, done) {
+  var layer = tileLayers.osm.osm;
+  var osmlint = 'missingroundabout';
+  var result = [];
+  for (var i = 0; i < layer.features.length; i++) {
+    var val = layer.features[i];
+
+    //if (.type == LineString val.properties.highway && val.geometry.coordinates[0] === val.geometry.coordinates[val.geometry.coordinates.length - 1]) {
+      
+    //}
+
+    if (val.geometry.type === 'LineString' && (  val.properties.highway === 'motorway' || val.properties.highway === 'trunk' || val.properties.highway === 'primary'||val.properties.highway === 'secondary' ||val.properties.highway === 'tertiary' || val.properties.highway === 'residential' && val.properties.highway === 'tertiary' && val.properties.highway === 'service' && val.properties.service === 'parking_aisle' )&& !val.properties.junction && !val.properties.oneway && val.geometry.coordinates[0][0] === val.geometry.coordinates[val.geometry.coordinates.length - 1][0]  && val.geometry.coordinates[0][1] === val.geometry.coordinates[val.geometry.coordinates.length - 1][1]) {
+
+      val.properties._osmlint = osmlint;
+      result.push(val);
+    }
+  }
+
+  if (result.length > 0) {
+    var fc = turf.featurecollection(result);
+    writeData(JSON.stringify(fc) + '\n');
+  }
+
+  done(null, null);
+};
