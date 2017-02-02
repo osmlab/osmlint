919f195a89a2d7b6401ac2ceebcfc56ac45fdd45
Rub21 <ruben@mapbox.com>
2017-02-02 10:27:01 -0500

    minor changes

--- validators/missingRoundabout/map.js
+++ validators/missingRoundabout/map.js
@@ -1,13 +1,39 @@
   'use strict';
   var turf = require('turf');
+  var _ = require('underscore');
 
   module.exports = function(tileLayers, tile, writeData, done) {
     var layer = tileLayers.osm.osm;
     var osmlint = 'missingroundabout';
     var result = [];
+    var majorRoads = {
+      'motorway': true,
+      'trunk': true,
+      'primary': true,
+      'secondary': true,
+      'tertiary': true,
+      'motorway_link': true,
+      'trunk_link': true,
+      'primary_link': true,
+      'secondary_link': true,
+      'tertiary_link': true
+    };
+    var minorRoads = {
+      'unclassified': true,
+      'residential': true
+    };
+
+    var preserveType = {};
+    preserveType = _.extend(preserveType, majorRoads);
+    preserveType = _.extend(preserveType, minorRoads);
+
     for (var i = 0; i < layer.features.length; i++) {
       var val = layer.features[i];
-      if (val.geometry.type === 'LineString' && (val.properties.highway === 'motorway' || val.properties.highway === 'trunk' || val.properties.highway === 'primary' || val.properties.highway === 'secondary' || val.properties.highway === 'tertiary' || val.properties.highway === 'residential' && val.properties.highway === 'tertiary' && val.properties.highway === 'service' && val.properties.service === 'parking_aisle') && !val.properties.junction && !val.properties.oneway && val.geometry.coordinates[0][0] === val.geometry.coordinates[val.geometry.coordinates.length - 1][0] && val.geometry.coordinates[0][1] === val.geometry.coordinates[val.geometry.coordinates.length - 1][1]) {
+      var coords = val.geometry.coordinates;
+      if (val.geometry.type === 'LineString' && preserveType[val.properties.highway] &&
+        !val.properties.junction && !val.properties.oneway &&
+        coords[0][0] === coords[coords.length - 1][0] &&
+        coords[0][1] === val.geometry.coordinates[coords.length - 1][1]) {
         val.properties._osmlint = osmlint;
         result.push(val);
       }
