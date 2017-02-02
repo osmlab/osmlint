b4540a3bdaee15566b9b48c749fb9c3eb8e890e2
Rub21 <ruben@mapbox.com>
2017-02-02 15:17:01 -0500

    fixing and test for missing roundabout
    update
    
    test for missing roundabout

--- test/missingRoundabout.test.js
+++ test/missingRoundabout.test.js
@@ -1,21 +1,22 @@
-  'use strict';
-  var test = require('tape');
-  var logInterceptor = require('log-interceptor');
-  var path = require('path');
-  var processors = require('../index.js');
-  var zoom = 12;
-  var missingRoundaboutTiles = path.join(__dirname, '/fixtures/missingroundabout.mbtiles');
-  var missingRoundaboutOpts = {
-    bbox: [-68.089142, -16.574503, -67.981339, -16.488271],
-    zoom: zoom
-  };
-  test('missingRoundabout', function(t) {
-    t.plan(1);
-    logInterceptor();
-    processors.missingRoundabout(missingRoundaboutOpts, missingRoundaboutTiles, function() {
-      var logs = logInterceptor.end();
-      var geoJSON = JSON.parse(logs);
-      t.equal(geoJSON.features[0].properties._osmlint, 'missingroundabout', 'Should be missingroundabout');
-      t.end();
-    });
+'use strict';
+var test = require('tape');
+var logInterceptor = require('log-interceptor');
+var path = require('path');
+var processors = require('../index.js');
+var zoom = 12;
+var missingRoundaboutTiles = path.join(__dirname, '/fixtures/missingroundabout.mbtiles');
+var missingRoundaboutOpts = {
+  bbox: [-77.624245, -9.6283377, -77.375679, -9.4299267],
+  zoom: zoom
+};
+test('missingRoundabout', function(t) {
+  t.plan(2);
+  logInterceptor();
+  processors.missingRoundabout(missingRoundaboutOpts, missingRoundaboutTiles, function() {
+    var logs = logInterceptor.end();
+    var geoJSON = JSON.parse(logs[0]);
+    t.equal(geoJSON.features[0].properties._osmlint, 'missingroundabout', 'Should be missingroundabout');
+    t.equal(geoJSON.features[0].properties.highway, 'primary', 'Should be primary');
+    t.end();
   });
+});
--- validators/missingRoundabout/map.js
+++ validators/missingRoundabout/map.js
@@ -1,48 +1,143 @@
-  'use strict';
-  var turf = require('turf');
-  var _ = require('underscore');
+'use strict';
+var turf = require('turf');
+var _ = require('underscore');
+var rbush = require('rbush');
+var flatten = require('geojson-flatten');
 
-  module.exports = function(tileLayers, tile, writeData, done) {
-    var layer = tileLayers.osm.osm;
-    var osmlint = 'missingroundabout';
-    var result = [];
-    var majorRoads = {
-      'motorway': true,
-      'trunk': true,
-      'primary': true,
-      'secondary': true,
-      'tertiary': true,
-      'motorway_link': true,
-      'trunk_link': true,
-      'primary_link': true,
-      'secondary_link': true,
-      'tertiary_link': true
-    };
-    var minorRoads = {
-      'unclassified': true,
-      'residential': true
-    };
+module.exports = function(tileLayers, tile, writeData, done) {
+  var layer = tileLayers.osm.osm;
+  var osmlint = 'missingroundabout';
+  var result = [];
+  var listOfHighways = {};
+  var highwaysBboxes = [];
+  var suspRoundaboutBboxes = [];
+  var majorRoads = {
+    'motorway': true,
+    'trunk': true,
+    'primary': true,
+    'secondary': true,
+    'tertiary': true,
+    'motorway_link': true,
+    'trunk_link': true,
+    'primary_link': true,
+    'secondary_link': true,
+    'tertiary_link': true
+  };
+  var minorRoads = {
+    'unclassified': true,
+    'residential': true
+  };
 
-    var preserveType = {};
-    preserveType = _.extend(preserveType, majorRoads);
-    preserveType = _.extend(preserveType, minorRoads);
-
-    for (var i = 0; i < layer.features.length; i++) {
-      var val = layer.features[i];
-      var coords = val.geometry.coordinates;
-      if (val.geometry.type === 'LineString' && preserveType[val.properties.highway] &&
-        !val.properties.junction && !val.properties.oneway &&
-        coords[0][0] === coords[coords.length - 1][0] &&
-        coords[0][1] === val.geometry.coordinates[coords.length - 1][1]) {
-        val.properties._osmlint = osmlint;
-        result.push(val);
+  var preserveType = {};
+  preserveType = _.extend(preserveType, majorRoads);
+  preserveType = _.extend(preserveType, minorRoads);
+  for (var i = 0; i < layer.features.length; i++) {
+    var val = layer.features[i];
+    if (val.geometry.type === 'MultiLineString' && preserveType[val.properties.highway]) {
+      var flat = flatten(val);
+      var id = val.properties['@id'] + 'L';
+      for (var f = 0; f < flat.length; f++) {
+        if (flat[f].geometry.type === 'LineString') {
+          var bboxM = turf.extent(flat[f]);
+          var idFlat = id + 'M' + f;
+          bboxM.push(idFlat);
+          highwaysBboxes.push(bboxM);
+          flat[f].properties = val.properties;
+          listOfHighways[idFlat] = flat[f];
+        }
       }
     }
-
-    if (result.length > 0) {
-      var fc = turf.featurecollection(result);
-      writeData(JSON.stringify(fc) + '\n');
+    if (preserveType[val.properties.highway] && val.geometry.type === 'LineString') {
+      var bboxHighway = turf.extent(val);
+      bboxHighway.push(val.properties['@id']);
+      listOfHighways[val.properties['@id']] = val;
+      var coords = val.geometry.coordinates;
+      if (coords[0][0] === coords[coords.length - 1][0] && coords[0][1] === coords[coords.length - 1][1]) {
+        suspRoundaboutBboxes.push(bboxHighway);
+      } else {
+        highwaysBboxes.push(bboxHighway);
+      }
     }
+  }
 
-    done(null, null);
-  };
+  var highwaysTree = rbush(highwaysBboxes.length);
+  highwaysTree.load(highwaysBboxes);
+  for (var k = 0; k < suspRoundaboutBboxes.length; k++) {
+    var roundaboutToEvaluate = listOfHighways[suspRoundaboutBboxes[k][4]];
+    if (isOval(roundaboutToEvaluate)) {
+      var coordgObjSerial = {};
+      var coordsRoundabout = roundaboutToEvaluate.geometry.coordinates;
+      for (var z = 0; z < coordsRoundabout.length; z++) {
+        coordgObjSerial[coordsRoundabout[z].join(',')] = {
+          type: null
+        };
+      }
+      var overlaps = highwaysTree.search(suspRoundaboutBboxes[k]);
+      for (var y = 0; y < overlaps.length; y++) {
+        var overlapHighway = listOfHighways[overlaps[y][4]];
+        var coordsOverlapHighway = overlapHighway.geometry.coordinates;
+        //if the highway is entering to roundabout
+        if (coordgObjSerial[coordsOverlapHighway[coordsOverlapHighway.length - 1].join(',')]) {
+          if (coordgObjSerial[coordsOverlapHighway[coordsOverlapHighway.length - 1].join(',')].type) {
+            coordgObjSerial[coordsOverlapHighway[coordsOverlapHighway.length - 1].join(',') + y] = {
+              type: 'entry'
+            };
+          }
+          coordgObjSerial[coordsOverlapHighway[coordsOverlapHighway.length - 1].join(',')].type = 'entry';
+        }
+        //if the highway is getting out from roundabout
+        if (coordgObjSerial[coordsOverlapHighway[0].join(',')]) {
+          if (coordgObjSerial[coordsOverlapHighway[0].join(',')].type) {
+            coordgObjSerial[coordsOverlapHighway[0].join(',') + y] = {
+              type: 'exit'
+            };
+          }
+          coordgObjSerial[coordsOverlapHighway[0].join(',')].type = 'exit';
+        }
+      }
+      var positions = _.values(coordgObjSerial);
+      var salidas = 0;
+      var entradas = 0;
+      for (var n = 0; n < positions.length; n++) {
+        if (positions[n].type === 'exit') {
+          ++salidas;
+        }
+        if (positions[n].type === 'entry') {
+          ++entradas;
+        }
+      }
+      if ((salidas > 1 && entradas > 1 && !roundaboutToEvaluate.properties.junction && !roundaboutToEvaluate.properties.oneway)) {
+        roundaboutToEvaluate.properties._osmlint = osmlint;
+        result.push(roundaboutToEvaluate);
+      }
+    }
+  }
+
+  if (result.length > 0) {
+    var fc = turf.featurecollection(result);
+    writeData(JSON.stringify(fc) + '\n');
+  }
+
+  done(null, null);
+};
+
+function isOval(highway) {
+  var centroidPt = turf.centroid(highway);
+  var points = turf.explode(highway);
+  var range = 1.2;
+  var flag = true;
+  var totalDistace = 0;
+  var distances = [];
+  for (var i = 0; i < points.features.length; i++) {
+    var distance = turf.distance(centroidPt, points.features[i], 'kilometers');
+    distances.push(distance);
+    totalDistace += distance;
+  }
+  var averageDistance = totalDistace / points.features.length;
+  for (var f = 0; f < distances.length; f++) {
+    if (distances[f] > averageDistance * range) {
+      flag = false;
+    }
+  }
+  return flag;
+}
