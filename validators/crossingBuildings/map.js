ee37fdaa2ccddaed74b28ac90b10105fa53cdbbc
Rub21 <ruben@mapbox.com>
2017-02-07 11:06:13 -0500

    crossing buildings

--- validators/crossingBuildings/map.js
+++ validators/crossingBuildings/map.js
@@ -5,56 +5,68 @@ var rbush = require('rbush');
 
 module.exports = function(tileLayers, tile, writeData, done) {
   var layer = tileLayers.osm.osm;
+  var overlapArea = 0.3;
   var buildings = {};
   var bboxes = [];
-
+  var output = {};
+  var osmlint = 'crossingbuildings';
   for (var i = 0; i < layer.features.length; i++) {
     var val = layer.features[i];
     if (val.geometry.type === 'Polygon' && val.properties.building) {
-
-      // var kinks = turf.kinks(val);
-      // if (kinks.intersections.features.length === 0) {
-      var bbox = turf.bbox(val);
-      bbox.push(val.properties['@id']);
-      bboxes.push(bbox);
-      buildings[val.properties['@id']] = val;
-      // }
+      var kinks = turf.kinks(val);
+      if (kinks && kinks.features.length === 0) {
+        var bbox = turf.bbox(val);
+        bbox.push(val.properties['@id']);
+        bboxes.push(bbox);
+        buildings[val.properties['@id']] = val;
+      } else if (kinks && kinks.features.length === 1) {
+        val.properties._osmlint = osmlint;
+        kinks.features[0].properties._osmlint = osmlint;
+        kinks.features[0].properties['@id'] = val.properties['@id'];
+        //save detection
+        output[val.properties['@id']] = val;
+        output[val.properties['@id'] + 'P'] = kinks.features[0];
+      } else if (kinks && kinks.features.length > 1) {
+        var multiCoords = [];
+        for (var k = 0; k < kinks.features.length; k++) {
+          multiCoords.push(kinks.features[k].geometry.coordinates);
+        }
+        var multiPt = turf.multiPoint(multiCoords);
+        multiPt.properties._osmlint = osmlint;
+        multiPt.properties['@id'] = val.properties['@id'];
+        //save detection
+        output[val.properties['@id']] = val;
+        output[val.properties['@id'] + 'M'] = multiPt;
+      }
     }
   }
-
-  // writeData(bboxes.length + '\n');
-
   var buildingTraceTree = rbush(bboxes.length);
   buildingTraceTree.load(bboxes);
-  var output = {};
   for (var z = 0; z < bboxes.length; z++) {
     var bbox = bboxes[z];
     var overlaps = buildingTraceTree.search(bbox);
-
     for (var k = 0; k < overlaps.length; k++) {
       var overlap = overlaps[k];
       if (overlap[4] !== bbox[4]) {
-        // writeData('=======================================\n');
-
         var intersect = turf.intersect(buildings[overlap[4]], buildings[bbox[4]]);
-        // if (intersect) {
-        //   writeData(JSON.stringify(intersect) + 'n');
-        // }
-        // var difference = turf.difference(buildings[overlap[4]], buildings[bbox[4]]);
-        // writeData('=====\n');
-
-        // if (difference) {
-        //   writeData(JSON.stringify(difference) + '\n');
-        // }
-        if (intersect && intersect.geometry.type === 'Polygon') {
+        if (intersect && (intersect.geometry.type === 'Polygon' || intersect.geometry.type === 'MultiPolygon')) {
           var area = turf.area(intersect);
-          if (area > 0.4) {
+          if (area > overlapArea) {
             var buildingA = buildings[overlap[4]];
-            buildingA.properties._osmlint = 'crossingbuildings';
+            buildingA.properties._osmlint = osmlint;
             var buildingB = buildings[bbox[4]];
-            buildingB.properties._osmlint = 'crossingbuildings';
+            buildingB.properties._osmlint = osmlint;
+            var points = turf.explode(intersect);
+            var multiPoint = turf.combine(points).features[0];
+            multiPoint.properties = {
+              _fromWay: buildingA.properties['@id'],
+              _toWay: buildingB.properties['@id'],
+              _osmlint: osmlint
+            };
+            //save detection
             output[overlap[4]] = buildingA;
             output[bbox[4]] = buildingB;
+            output[overlap[4] + bbox[4] + 'M'] = multiPoint;
           }
         }
       }
@@ -62,7 +74,6 @@ module.exports = function(tileLayers, tile, writeData, done) {
   }
 
   var result = _.values(output);
-
   if (result.length > 0) {
     var fc = turf.featureCollection(result);
     writeData(JSON.stringify(fc) + '\n');
