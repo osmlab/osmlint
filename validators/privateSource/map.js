9d7938a78bf96cf5eea29a9f06a18eb272fd4f59
Rub21 <ruben@mapbox.com>
2017-02-16 17:10:54 -0500

    detect  unallowed source

--- validators/privateSource/map.js
+++ validators/privateSource/map.js
@@ -5,28 +5,30 @@ module.exports = function(tileLayers, tile, writeData, done) {
   var layer = tileLayers.osm.osm;
   var osmlint = 'privatesource';
   var result = [];
-  var unallowedSource = {
-    'Google Maps': true,
-    'Google Satellite': true,
-    'Google Earth': true,
-    'Google Street View': true,
-    'Waze': true,
-    'HERE Maps': true,
-    'Geodata from Wikipedia': true,
-    'Wikimapia': true,
-    'Malfreemaps': true,
-    'Malsingmaps': true
-  };
+  var unallowedSource = [
+    'google',
+    'nokia',
+    'here',
+    'waze',
+    'apple',
+    'tomtom',
+    'import',
+    'wikimapia'
+  ];
   for (var i = 0; i < layer.features.length; i++) {
     var val = layer.features[i];
-    if (val.properties.source && unallowedSource[val.properties.source]) {
-      val.properties._osmlint = osmlint;
-      result.push(val);
+    if (val.properties.source) {
+      for (var k = 0; k < unallowedSource.length; k++) {
+        if (val.properties.source.toLowerCase().includes(unallowedSource[k])) {
+          val.properties._osmlint = osmlint;
+          result.push(val);
+        }
+      }
     }
   }
 
   if (result.length > 0) {
-    var fc = turf.featurecollection(result);
+    var fc = turf.featureCollection(result);
     writeData(JSON.stringify(fc) + '\n');
   }
 
