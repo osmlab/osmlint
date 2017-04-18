3c208766d6d38cb814aea75282c16a8f95c98044
Rub21 <ruben@mapbox.com>
2017-04-18 17:32:10 -0500

    double places -cities

--- validators/doubledCity/map.js
+++ validators/doubledCity/map.js
@@ -3,61 +3,67 @@ var turf = require('turf');
 var _ = require('underscore');
 
 module.exports = function(tileLayers, tile, writeData, done) {
-    var layer = tileLayers.osm.osm;
-    var osmlint = 'doubledcity';
-    var result = [];
-    var objs = {};
-    var places = {
-      'suburb': true,
-      'hamlet': true,
-      'neighbourhood': true
-    };
+  var layer = tileLayers.osm.osm;
+  var osmlint = 'doubledcity';
+  var result = [];
+  var objs = {};
+  var places = {
+    'country': true,
+    'state': true,
+    'region': true,
+    'province': true,
+    'district': true,
+    'county': true,
+    'municipality': true,
+    'city': true,
+    'borough': true,
+    'suburb': true,
+    'quarter': true,
+    'city_block': true,
+    'plot': true,
+    'twon': true,
+    'village': true,
+    'hamlet': true,
+    'farm': true,
+    'allotments': true,
+    'neighbourhood': true
+  };
 
-    for (var i = 0; i < layer.features.length; i++) {
-      var val = layer.features[i];
-      if (val.properties.place && places[val.properties.place] && (val.geometry.type === 'Point' || val.geometry.type === 'Polygon') && val.properties.name) {
-        var pname = val.properties.name.toLowerCase();
-        if (objs[pname]) {
-          objs[pname].push(val);
-          objs[pname] = [val];
-        }
-      }
-
-
-      _.each(objs, function(v, k) {
-        if (v.length > 1) {
-          // writeData(JSON.stringify(v.length) + '\n')
-
-          var coordenadas = [];
-          for (var i = 0; i < v.length; i++) {
-            coordenadas.push(v[i].geometry.coordinates);
-          }
-
-          writeData(JSON.stringify(coordenadas) + '\n')
-
-        }
-
-
-      });
-
-
-      if (result.length > 0) {
-        var fc = turf.featureCollection(result);
-        //writeData(JSON.stringify(fc) + '\n');
-      }
-
-      done(null, null);
-    };
-
-    function getDistance(o1, o2) {
-      var p, pp;
-      if (o1.geometry.type === 'Polygon') {
-        pp = turf.centroid(o1);
-        p = o2;
+  for (var i = 0; i < layer.features.length; i++) {
+    var val = layer.features[i];
+    if (val.properties.place && places[val.properties.place] && (val.geometry.type === 'Point' || val.geometry.type === 'Polygon') && val.properties.name) {
+      var pname = val.properties.place + '-' + val.properties.name.toLowerCase();
+      if (objs[pname]) {
+        objs[pname].push(val);
       } else {
-        pp = turf.centroid(o2);
-        p = o1;
+        objs[pname] = [val];
       }
-      return turf.distance(pp, p, 'kilometers');
     }
-    
+  }
+  _.each(objs, function(v, k) {
+    var coordPoints = [];
+    if (v.length > 1) {
+      for (var i = 0; i < v.length; i++) {
+        if (v[i].geometry.type === 'Polygon') {
+          var points = turf.explode(v[i]);
+          for (var n = 0; n < points.features.length; n++) {
+            coordPoints.push(points.features[n].geometry.coordinates)
+          }
+        } else if (v[i].geometry.type === 'Point') {
+          coordPoints.push(v[i].geometry.coordinates);
+        }
+      }
+      var multiPt = turf.multiPoint(coordPoints);
+      multiPt.properties = v[0].properties;
+      result.push(multiPt);
+    }
+  });
+
+  if (result.length > 0) {
+    var fc = turf.featureCollection(result);
+    writeData(JSON.stringify(fc) + '\n');
+  }
+
+  done(null, null);
+
+};
