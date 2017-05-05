659d4a8f424c7a9cc72efa286abcdf5e019a272e
Rub21 <ruben@mapbox.com>
2017-05-05 15:26:18 -0500

    add selfintersectingwaterway

---
+++ test/selfIntersectingWaterway.test.js
@@ -0,0 +1,28 @@
+'use strict';
+var test = require('tape');
+var logInterceptor = require('log-interceptor');
+var path = require('path');
+var processors = require('../index.js');
+var zoom = 12;
+var selfintersectingwaterwayTiles = path.join(__dirname, '/fixtures/selfintersectingwaterway.mbtiles');
+var commonOpts = {
+  bbox: [-74.261398, -13.217553, -74.154968, -13.112583],
+  zoom: zoom
+};
+
+test('selfintersectingwaterway', function(t) {
+  t.plan(4);
+  logInterceptor();
+  processors.selfIntersectingWaterway(commonOpts, selfintersectingwaterwayTiles, function() {
+    var logs = logInterceptor.end();
+    for (var i = 0; i < logs.length; i++) {
+      var geoJSON = JSON.parse(logs[i]);
+      t.comment('Pass: ' + (i + 1));
+      if (geoJSON.features.length > 0) {
+        t.equal(geoJSON.features[0].properties._osmlint, 'selfintersectingwaterway', 'Should be selfintersectingwaterway');
+        t.equal(geoJSON.features[0].geometry.type, 'MultiPoint', 'Should be  MultiPoint');
+      }
+    }
+    t.end();
+  });
+});
