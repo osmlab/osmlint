d0291cad2a8438b518f604d34be088699b0f939b
Rub21 <ruben@mapbox.com>
2017-06-29 11:23:30 -0700

    wikidatanoenglishname

--- validators/wikiDataNoEnglishName/map.js
+++ validators/wikiDataNoEnglishName/map.js
@@ -5,14 +5,13 @@ module.exports = function(tileLayers, tile, writeData, done) {
   var layer = tileLayers.osm.osm;
   var osmlint = 'wikidatanoenglishname';
   var result = [];
+
   for (var i = 0; i < layer.features.length; i++) {
     var val = layer.features[i];
-
     if (val.geometry.type == 'Point' && val.properties.place && val.properties.wikidata && !val.properties['name:en']) {
       val.properties._osmlint = osmlint;
       result.push(val);
     }
-
   }
 
   if (result.length > 0) {
