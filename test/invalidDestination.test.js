e6bc0dd7f68b7abcb72327c2d7775a0c7279ec36
Rub21 <ruben@mapbox.com>
2017-08-03 18:35:10 -0500

    update readme and change mbtiles for invalid motorway junction

--- test/invalidDestination.test.js
+++ test/invalidDestination.test.js
@@ -5,7 +5,7 @@ var path = require('path');
 var processors = require('../index.js');
 var zoom = 12;
 var mbtile = path.join(__dirname, '/fixtures/invaliddestination.mbtiles');
-var turnLaneOpts = {
+var opts = {
   bbox: [-122.66630, 37.098181, -121.39189, 37.969373],
   zoom: zoom
 };
@@ -13,7 +13,7 @@ var turnLaneOpts = {
 test('invalidDestination', function(t) {
   t.plan(2);
   logInterceptor();
-  processors.invalidDestination(turnLaneOpts, mbtile, function() {
+  processors.invalidDestination(opts, mbtile, function() {
     var logs = logInterceptor.end();
     var geoJSON = JSON.parse(logs[0]);
     t.equal(geoJSON.features[0].geometry.type, 'LineString', 'Should be LineString');
