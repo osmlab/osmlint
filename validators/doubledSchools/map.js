545a6bf527c871c8d05b4c0c4064239355bae1dc
Rub21 <rub2106@gmail.com>
2018-05-10 17:46:54 -0500

    compare amenity=school point and polygons

--- validators/doubledSchools/map.js
+++ validators/doubledSchools/map.js
@@ -15,29 +15,61 @@ module.exports = function(tileLayers, tile, writeData, done) {
   var osmlint = 'doubledpoi';
   for (var i = 0; i < layer.features.length; i++) {
     var val = layer.features[i];
-    if (val.properties.amenity && keepFeatures[val.properties.amenity] && val.properties.name && (val.geometry.type === 'Point' || val.geometry.type === 'Polygon')) {
+    if (
+      val.properties.amenity &&
+      keepFeatures[val.properties.amenity] &&
+      val.properties.name &&
+      (val.geometry.type === 'Point' || val.geometry.type === 'Polygon')
+    ) {
       var id = val.properties['@id'];
+      if (val.geometry.type === 'Polygon') {
+        var centroid = turf.centroid(val);
+        centroid.properties = val.properties;
+        val = centroid;
+      }
       //The next layer should be replaced according to the country, This example was made for Peru.
-      val.properties.name = val.properties.name.toLowerCase().replace(/\s/g, '').replace('institucióneducativa', '').replace('inicialno.', '').replace('no.', '');
+      val.properties.name = val.properties.name
+        .toLowerCase()
+        .replace(/\s/g, '')
+        .replace('institucióneducativa', '')
+        .replace('inicialno.', '')
+        .replace('no.', '');
       if (!objnames[id]) {
         objnames[id] = val;
       }
     }
   }
-  
+
   for (var poiA in objnames) {
     for (var poiB in objnames) {
       var name = objnames[poiA].properties.name;
       var name2 = objnames[poiB].properties.name;
-      if (getDistance(objnames[poiA], objnames[poiB]) < 2 && objnames[poiA].properties.amenity === objnames[poiB].properties.amenity) {
-        if ((poiB !== poiA && _.isNumber(name) && _.isNumber(name2) && name === name2) ||
-          (poiB !== poiA && !_.isNumber(name) && !_.isNumber(name2) && levenshtein.get(name, name2) < 2)) {
+      if (
+        getDistance(objnames[poiA], objnames[poiB]) < 0.5 &&
+        objnames[poiA].properties.amenity === objnames[poiB].properties.amenity
+      ) {
+        if (
+          (poiB !== poiA &&
+            _.isNumber(name) &&
+            _.isNumber(name2) &&
+            name === name2) ||
+          (poiB !== poiA &&
+            !_.isNumber(name) &&
+            !_.isNumber(name2) &&
+            levenshtein.get(name, name2) < 2)
+        ) {
           var poisIds = poiB + '' + poiA;
           if (poiA > poiB) {
             poisIds = poiA + '' + poiB;
           }
-          var multiPt = turf.multiPoint([objnames[poiB].geometry.coordinates, objnames[poiA].geometry.coordinates]);
-          multiPt.properties = Object.assign(objnames[poiB].properties, objnames[poiA].properties);
+          var multiPt = turf.multiPoint([
+            objnames[poiB].geometry.coordinates,
+            objnames[poiA].geometry.coordinates
+          ]);
+          multiPt.properties = Object.assign(
+            objnames[poiB].properties,
+            objnames[poiA].properties
+          );
           resultPoints[poisIds] = multiPt;
         }
       }
