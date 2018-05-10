b42be900763f9dc5b915486e08dcf7cff0d5764c
Rub21 <rub2106@gmail.com>
2018-05-10 16:12:44 -0500

    add doubled Schools

---
+++ validators/doubledSchools/index.js
@@ -0,0 +1,22 @@
+'use strict';
+var tileReduce = require('@mapbox/tile-reduce');
+var path = require('path');
+
+module.exports = function(opts, mbtilesPath, callback) {
+  tileReduce({
+    bbox: opts.bbox,
+    zoom: opts.zoom,
+    map: path.join(__dirname, '/map.js'),
+    sources: [
+      {
+        name: 'osm',
+        mbtiles: mbtilesPath,
+        raw: false
+      }
+    ]
+  })
+    .on('reduce', function() {})
+    .on('end', function() {
+      callback && callback();
+    });
+};
